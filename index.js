61293381b5446b102f0392cc9bd5fbf4501e0543
catheali <aliciacatherinealencar@gmail.com>
2022-10-26 18:03:54 -0300

    instalando o express

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const port = 8000
+
+// const produtoRota = require("./controllers/produto/router.js")
+// const usuarioRota = require("./controllers/user/router.js")
+// const carrinhoRota = require("./controllers/carrinho/router.js")
+
+
+app.get('/', (req, res) => {
+  res.send('A documentação da api')
+})
+
+app.use(express.json())
+
+// app.use('/', produtoRota)
+// app.use('/', usuarioRota)
+// app.use('/', carrinhoRota)
+
+
+app.use((req, res) => {
+  res.status(404).send("{message: rota não encontrada}")
+})
+
+app.listen(port, () => {
+  console.log(`Rodando no link http://localhost:${port}`)
+})
