d44bd8a85d6ca50b20d5656f70167a877f22fe9a
catheali <aliciacatherinealencar@gmail.com>
2022-10-26 18:53:57 -0300

    promocoes

--- controllers/produto/router.js
+++ controllers/produto/router.js
@@ -3,10 +3,12 @@ const route = express.Router();
 
 const produto = require("./produto");
 
-route.get("/produto", (req, res) => {
-  res.send(produto.listar());
-});
-
 route.get("/promocoes", (req, res) => {
   res.send(produto.listPromo());
 });
+
+
+
+
+
+module.exports = route;
