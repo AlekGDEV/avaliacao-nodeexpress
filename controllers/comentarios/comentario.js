5a16801b92516e83868ea99cb5fa84cab8922ef1
davimaia85 <davilnd8@gmail.com>
2022-10-27 04:35:06 -0300

    comntario.js

--- controllers/comentarios/comentario.js
+++ controllers/comentarios/comentario.js
@@ -4,3 +4,12 @@ function listar(produtoIdParam) {
     return JSON.parse(fs.readFileSync(__dirname + '/comentario.json').toString())
         .filter(({ produtoId }) => produtoId === produtoIdParam);
 }
+
+function listarAutor(produtoIdParam, autorParam = '') {
+    return listar(produtoIdParam)
+        .filter(({ nomeDoAutor }) => nomeDoAutor.toLowerCase().indexOf(autorParam.toLowerCase()) >= 0)
+}
+module.exports = {
+    listar,
+    listarAutor
+}
